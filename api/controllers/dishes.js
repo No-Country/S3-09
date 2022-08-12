c77575a5f029893b85b369937b8efaeb70c074a5
Jon_menez <joanf.gimenez@gmail.com>
2022-08-12 15:52:49 -0300

    [ADD] create dishes

--- api/controllers/dishes.js
+++ api/controllers/dishes.js
@@ -1,11 +1,28 @@
 const Dish = require('../models/dish');
 
 const getDishes = async (req, res) => {
-    const dishes = await Dish.findAll();
+    const dishes = await Dish.findAll({
+        include: ['restaurant']
+    });
     res.json(dishes);
 }
 
+const createDish = async (req, res) => {
+    const { name, description, price, restaurant_id } = req.body;
+    const dish = new Dish({
+        name,
+        description,
+        price,
+        restaurant_id
+    });
+    await dish.save();
+    res.json({
+        msg: 'Dish created',
+        dish
+    });
+}
 
 module.exports = {
-    getDishes
+    getDishes,
+    createDish
 }
