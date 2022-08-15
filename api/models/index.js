40a88034900bc09b4478d4c1fe6f245f789835cd
Jon_menez <joanf.gimenez@gmail.com>
2022-08-15 20:12:33 -0300

    [ADD] index to imrpove imports

---
+++ api/models/index.js
@@ -0,0 +1,13 @@
+const Booking = require('../models/booking');
+const Card = require('../models/card');
+const Dish = require('../models/dish');
+const Restaurant = require('../models/restaurant');
+const User = require('../models/user');
+
+module.exports = {
+    Booking,
+    Card,
+    Dish,
+    Restaurant,
+    User,
+}
