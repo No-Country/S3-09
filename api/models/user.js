6a6918dff86be61245c5c1e651ba29b27ab73501
Jon_menez <joanf.gimenez@gmail.com>
2022-08-17 15:47:53 -0300

    [ADD] associations to Favorites & Restaurants

--- api/models/user.js
+++ api/models/user.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database/connection');
-const { Booking, Card } = require('../models/index');
+const { Booking, Card, Favorite, Restaurant } = require('../models/index');
 const User = sequelize.define('user', {
 
     name: {
@@ -35,10 +35,20 @@ const User = sequelize.define('user', {
 );
 
 //Associations
+//User has many Bookings
 User.hasMany(Booking, { foreignKey: 'user_id', as: 'bookings' });
 Booking.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
 
+//User has many Cards
 User.hasMany(Card, { foreignKey: 'user_id', as: 'cards' });
 Card.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+
+//User has many Favorites
+User.hasMany(Favorite, { foreignKey: 'user_id', as: 'favorites' });
+Favorite.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+
+//Favorite has one Restaurant
+Restaurant.hasMany(Favorite, { foreignKey: 'restaurant_id', as: 'restaurant' });
+Favorite.belongsTo(Restaurant, { foreignKey: 'restaurant_id', as: 'favorite' });
 
 module.exports = User;
