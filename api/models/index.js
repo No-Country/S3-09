0f03c11bad351398fb1bbc98067d7ea501209d93
Jon_menez <joanf.gimenez@gmail.com>
2022-08-17 15:47:00 -0300

    [ADD] favorite model

--- api/models/index.js
+++ api/models/index.js
@@ -1,6 +1,7 @@
 const Booking = require('../models/booking');
 const Card = require('../models/card');
 const Dish = require('../models/dish');
+const Favorite = require('../models/favorite');
 const Restaurant = require('../models/restaurant');
 const User = require('../models/user');
 
@@ -8,6 +9,7 @@ module.exports = {
     Booking,
     Card,
     Dish,
+    Favorite,
     Restaurant,
     User,
 }
