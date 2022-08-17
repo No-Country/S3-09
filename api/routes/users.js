54be05849501edf0c809a9c94b98afa081877a20
Jon_menez <joanf.gimenez@gmail.com>
2022-08-16 22:33:52 -0300

    [ADD] route to get user info

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { validateInputs, validUserID } = require('../middlewares/');
-const { getUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/users');
-const { existEmail } = require('../helpers/validateDB');
+const { getUsers, getUserById, createUser, updateUser, deleteUser, getInfoByUserId } = require('../controllers/users');
+const { existEmail, existUsername } = require('../helpers/validateDB');
 
 const router = Router();
 
@@ -12,11 +12,18 @@ router.get('/:id', [
     validUserID,
 ], getUserById);
 
+
+router.get('/:id/:userInfo', [
+    validUserID,
+], getInfoByUserId);
+
+
 router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is not valid').isEmail(),
     check('password', 'Password must be at least 6 characters long').isLength({ min: 6 }),
     check('email').custom(existEmail),
+    check('username', 'Username is required').custom(existUsername),
     validateInputs
 ], createUser);
 
