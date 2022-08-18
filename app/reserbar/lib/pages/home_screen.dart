7177299d5f8eff4821135fd3cdd217ab1a683588
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-08-18 01:07:13 -0300

    feat(Home): cards with parameters and example data

--- app/reserbar/lib/pages/home_screen.dart
+++ app/reserbar/lib/pages/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+//import 'dart:convert';
 
 import '../ui/bar_card.dart';
+
+//Map demoData= json.decode(  );
+
 
 final List<Map<String, String>> bar = [
   {
@@ -9,69 +13,82 @@ final List<Map<String, String>> bar = [
     'horario': '8:00 - 12:00',
     'imagen':
         'https://img.freepik.com/foto-gratis/gran-tiro-botellas-vasos-vitrina-bar-hotel-scandic-copenhague-dinamarca_181624-2920.jpg?w=1060&t=st=1660362661~exp=1660363261~hmac=94f9a99cc8912d6fc9a6fbd2b544ee3e29e2a8ef1015805a180bbd236ec88fd5',
+    'puntos': '5',
+    'favorito': 'true',
   },
   {
-    'nombre': 'Saints Bar',
-    'direccion': 'Fitz Roy 1050',
-    'horario': '8:00 - 12:00',
+    'nombre': 'Chulo Bar',
+    'direccion': 'Belgrano 2020',
+    'horario': '20:00 - 02:00',
     'imagen':
-        'https://img.freepik.com/foto-gratis/gran-tiro-botellas-vasos-vitrina-bar-hotel-scandic-copenhague-dinamarca_181624-2920.jpg?w=1060&t=st=1660362661~exp=1660363261~hmac=94f9a99cc8912d6fc9a6fbd2b544ee3e29e2a8ef1015805a180bbd236ec88fd5',
+        'https://rapidapi.com/blog/wp-content/uploads/2018/06/shangyou-shi-543887-unsplash-1024x683.jpg',
+    'puntos': '3.2',
+    'favorito': 'false',
   },
   {
-    'nombre': 'Saints Bar',
-    'direccion': 'Fitz Roy 1050',
-    'horario': '8:00 - 12:00',
+    'nombre': 'La Factoria',
+    'direccion': 'Cucha Cucha 3250',
+    'horario': '18:00 - 24:00',
     'imagen':
-        'https://img.freepik.com/foto-gratis/gran-tiro-botellas-vasos-vitrina-bar-hotel-scandic-copenhague-dinamarca_181624-2920.jpg?w=1060&t=st=1660362661~exp=1660363261~hmac=94f9a99cc8912d6fc9a6fbd2b544ee3e29e2a8ef1015805a180bbd236ec88fd5',
+        'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    'puntos': '4.4',
+    'favorito': 'false',
   },
   {
-    'nombre': 'Saints Bar',
-    'direccion': 'Fitz Roy 1050',
-    'horario': '8:00 - 12:00',
+    'nombre': 'Vida Frita',
+    'direccion': 'Recoleta 111',
+    'horario': '8:00 - 24:00',
     'imagen':
-        'https://img.freepik.com/foto-gratis/gran-tiro-botellas-vasos-vitrina-bar-hotel-scandic-copenhague-dinamarca_181624-2920.jpg?w=1060&t=st=1660362661~exp=1660363261~hmac=94f9a99cc8912d6fc9a6fbd2b544ee3e29e2a8ef1015805a180bbd236ec88fd5',
+        'https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'puntos': '4.5',
+    'favorito': 'true', 
   },
   {
-    'nombre': 'Saints Bar',
-    'direccion': 'Fitz Roy 1050',
-    'horario': '8:00 - 12:00',
+    'nombre': 'Le Vered',
+    'direccion': 'Teodoro Planas 1643',
+    'horario': '11:00 - 15:00',
     'imagen':
-        'https://img.freepik.com/foto-gratis/gran-tiro-botellas-vasos-vitrina-bar-hotel-scandic-copenhague-dinamarca_181624-2920.jpg?w=1060&t=st=1660362661~exp=1660363261~hmac=94f9a99cc8912d6fc9a'
+        'https://images.unsplash.com/photo-1557694705-5cac01cf94a5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    'puntos': '4.6',
+    'favorito': 'true',
   }
 ];
 
-final String lala = bar[0]['nombre'].toString();
+
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
        body: 
-      //ListView(children: [
-      //   const ListTile(
-      //     leading: Icon(Icons.arrow_back_ios_new),
-      //     title: Text('Nueva Reserva'),
-      //     subtitle: Text('Elegí el Lugar'),
-      //   ),
-        ListView.builder(
-          padding: const EdgeInsets.all(18),
-          itemCount: 4,
-          itemBuilder: (BuildContext context, int index) {
-            return Center(
-              child: Container(
-                padding: const EdgeInsets.all(2),
-                child: const BarCard(
-                    title: 'Saints Bar',
-                    direction: 'Fitz Roy 1050',
-                    hour: '8:00 - 12:00',
-                    thumbnail:
-                        'https://img.freepik.com/foto-gratis/gran-tiro-botellas-vasos-vitrina-bar-hotel-scandic-copenhague-dinamarca_181624-2920.jpg?w=1060&t=st=1660362661~exp=1660363261~hmac=94f9a99cc8912d6fc9a'),
-              ),
-            );
-          },
-        ),
-      //]      ),
+       _createList(),
+       
     );
   }
+
+  Widget _createList() {
+  
+    return 
+      ListView.builder(
+        padding: const EdgeInsets.all(18),
+        itemCount: bar.length,
+        itemBuilder: (BuildContext context, int index) {
+      
+          return Container(
+            padding: const EdgeInsets.all(2),
+            child: BarCard(
+              title:  '${bar[index]['nombre']}',
+              direction: '${bar[index]['direccion']}',
+              hour: '${bar[index]['horario']}',
+              thumbnail: '${bar[index]['imagen']}',
+              points: '${bar[index]['puntos']}',
+              favorite: '${bar[index]['favorito']}',
+            ),
+          );
+        },
+      );
+  }
 }
+        
--- app/reserbar/lib/ui/bar_card.dart
+++ app/reserbar/lib/ui/bar_card.dart
@@ -7,100 +7,69 @@ class BarCard extends StatelessWidget {
     required this.title,
     required this.direction,
     required this.hour,
+    required this.points,
+    required this.favorite,
   }) : super(key: key);
   final String thumbnail;
   final String title;
   final String direction;
   final String hour;
+  final String points;
+  final String favorite;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        semanticContainer: true,
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        elevation: 10.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Row(crossAxisAlignment: CrossAxisAlignment.center, children: const [
-              Expanded(
-                flex: 6,
-                child: FadeInImage(
-                  placeholder: NetworkImage(''),
-                  image: NetworkImage(
-                      'https://img.freepik.com/foto-gratis/gran-tiro-botellas-vasos-vitrina-bar-hotel-scandic-copenhague-dinamarca_181624-2920.jpg?w=1060&t=st=1660362661~exp=1660363261~hmac=94f9a99cc8912d6fc9a6fbd2b544ee3e29e2a8ef1015805a180bbd236ec88fd5'),
-                  fit: BoxFit.fitWidth,
-                  height: 100,
-                  alignment: Alignment.center,
-                ),
-              ),
-            ]),
-            Container(
-              padding: const EdgeInsets.all(10.0),
-              color: const Color.fromRGBO(20, 57, 129, 0.9),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 8,
-                    child: _BarCardData(
-                      title: title,
-                      direction: direction,
-                      hour: hour,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Card(
-                      color: const Color.fromRGBO(217, 217, 217, 1),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 4.0, vertical: 1.0),
-                        child: Row(children: const [
-                          Icon(
-                            Icons.favorite_border,
-                            color: Color.fromRGBO(233, 33, 189, 1),
-                            size: 25,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.all(3.0),
-                            child: Text('4.4',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                )),
-                          ),
-                        ]),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, '/home', arguments: title),
+      child: Card(
+            semanticContainer: true,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            elevation: 10.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.0),
             ),
-          ],
-        ));
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+                  _barCardTopImage(),
+                ]),
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  color: const Color.fromRGBO(20, 57, 129, 0.9),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 8,
+                        child: _barCardData(),
+                      ),
+                      _barCardFavourite(),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+    );
   }
-}
 
-class _BarCardData extends StatelessWidget {
-  const _BarCardData({
-    Key? key,
-    required this.title,
-    required this.direction,
-    required this.hour,
-  }) : super(key: key);
+  Widget _barCardTopImage() {
+    return Expanded(
+      flex: 6,
+      child: FadeInImage.assetNetwork(
+        placeholderCacheWidth: 10,
+        placeholderCacheHeight: 10,
+        placeholder: 'assets/loading.gif',
+        image: thumbnail,
+        fit: BoxFit.fitWidth,
+        height: 100,
+        alignment: Alignment.center,
+        fadeInDuration: const Duration(milliseconds: 200),
+      ),
+    );
+  }
 
-  final String title;
-  final String direction;
-  final String hour;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _barCardData() {
     return Padding(
       padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
       child: Column(
@@ -128,4 +97,31 @@ class _BarCardData extends StatelessWidget {
       ),
     );
   }
+
+  Widget _barCardFavourite() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Card(
+        color: const Color.fromRGBO(217, 217, 217, 1),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 1.0),
+          child: Row(
+            children: [ 
+              favorite=='true' ? const Icon(Icons.favorite, color: Color.fromRGBO(233, 33, 189, 1), size: 25.0,) : const Icon(Icons.favorite_border, color: Color.fromRGBO(233, 33, 189, 1), size: 20.0,),
+              Padding(
+                padding: EdgeInsets.all(3.0),
+                child: Text(points,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+            
 }
