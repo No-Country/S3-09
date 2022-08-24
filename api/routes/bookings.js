53dd86f5ea78efcf48dbdae1162cce5f938a79d5
Jon_menez <joanf.gimenez@gmail.com>
2022-08-24 18:24:31 -0300

    [ADD] middlewares to post request

--- api/routes/bookings.js
+++ api/routes/bookings.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { validateInputs } = require('../middlewares/');
+const { validateInputs, validJWT, validRestoID } = require('../middlewares/');
 const { getBookings, getBooking, createBooking, deleteBooking, updateBooking } = require('../controllers/bookings');
 const { checkTimeFormat } = require('../helpers/validateDB');
 
@@ -11,15 +11,17 @@ routes.get('/', getBookings);
 routes.get('/:id', getBooking);
 
 routes.post('/', [
-    check('clients', 'Clients are required').not().isEmpty(),
-    check('time', 'Time is required').not().isEmpty(),
+    validJWT,
+    validRestoID,
+    check('clients', 'Clients are required').not().isEmpty().isInt(),
+    check('clients', 'Clients must be intenger').isInt(),
     check('time').custom(checkTimeFormat),
     validateInputs
 ], createBooking);
 
 routes.put('/:id', [
-    check('clients', 'Clients are required').not().isEmpty(),
-    check('time', 'Time is required').not().isEmpty(),
+    check('clients', 'Clients are required').not().isEmpty().isInt(),
+    check('clients', 'Clients must be intenger').isInt(),
     check('time').custom(checkTimeFormat),
     validateInputs
 ], updateBooking);
