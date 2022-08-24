ed284a80dddc415e4d8babab9cfc50e5b9e25af3
Jon_menez <joanf.gimenez@gmail.com>
2022-08-24 18:16:11 -0300

    [UPDATE] create & update dishes

--- api/controllers/dishes.js
+++ api/controllers/dishes.js
@@ -14,14 +14,45 @@ const getDishes = async (req, res) => {
 }
 
 const createDish = async (req, res) => {
-    const { name, description, price, restaurant_id } = req.body;
+    const { name, img } = req.body;
+
+    const newImg = img
+
     const dish = new Dish({
         name,
-        description,
-        price,
-        restaurant_id
+        img: newImg || 'https://res.cloudinary.com/jonmenez/image/upload/v1661363859/Diiner/b86c6efe87c5725c0c8b3e76523886bd_nen7ry.jpg'
     });
+
     await dish.save();
+    res.json({
+        msg: 'Dish created',
+        dish
+    });
+}
+
+const updateDish = async (req, res) => {
+    const { id } = req.params
+    const { name } = req.body;
+
+    const dish = await Dish.findByPk(id)
+
+    if (!dish) {
+        res.status(404).json({
+            msg: 'dish not found'
+        })
+    }
+
+    let newImg = dish.img
+
+    if (newImg === dish.img) {
+        newImg = img
+    }
+
+    await dish.update({
+        name,
+        img: newImg
+    });
+
     res.json({
         msg: 'Dish created',
         dish
@@ -30,5 +61,6 @@ const createDish = async (req, res) => {
 
 module.exports = {
     getDishes,
-    createDish
+    createDish,
+    updateDish
 }
