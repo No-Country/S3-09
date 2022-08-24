2bbab318ff6ce1df30c62428e614cbf8cee9aec8
Jon_menez <joanf.gimenez@gmail.com>
2022-08-24 18:21:30 -0300

    [ADD] conditional statement

--- api/middlewares/validResto.js
+++ api/middlewares/validResto.js
@@ -2,10 +2,16 @@ const Restaurant = require('../models/restaurant');
 
 const validRestoID = async (req, res, next) => {
     const { id } = req.params;
+    const { restaurant_id } = req.body
 
-    const restaurant = await Restaurant.findByPk(id);
+    const restaurant = await Restaurant.findByPk(id || restaurant_id);
 
     if (!restaurant) {
+        if (!id) {
+            return res.status(404).json({
+                msg: 'restaurant_id is required'
+            });
+        }
         return res.status(404).json({
             msg: 'Restaurant not found'
         });
