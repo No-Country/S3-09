2b3ad9243c08138e65e8fdde5f921e9f09616de2
LZDEV <81957303+Lestradez@users.noreply.github.com>
2022-09-03 07:19:42 -0300

    new Card validation

--- reserbar/src/pages/newCard/newCard.jsx
+++ reserbar/src/pages/newCard/newCard.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 
-
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
@@ -20,6 +19,10 @@ import CustomInputForm from "../../components/customInputForm/customInputForm";
 import { addNewCardReducer } from "../../redux/reducers/addNewCardReducer";
 
 const NewCard = () => {
+    const dateFormat = (date) => {
+        return /^(0[1-9]|1[0-2])\/([0-9]{2})$/g.test(date)
+    }
+    console.log(dateFormat("12/22"));
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const { handleChange, values, errors, handleSubmit } = useFormik({
@@ -29,13 +32,24 @@ const NewCard = () => {
             card_expires: "",
             card_cvv: "",
         },
-        // validationSchema: Yup.object().shape({
-        //     card_name: Yup.string()
-        //         .mail("ingrese un nombre válido")
-        //         .max(40)
-        //         .required("Debe ingresar un nombre"),
-            
-        // }),
+        validationSchema: Yup.object().shape({
+            card_name: Yup.string()
+                .min(10, "Debe tener al menos 10 caracteres")
+                .max(40, "Debe tener menos de 40 caracteres")
+                .required("Debe ingresar un nombre"),
+            card_number: Yup.number()
+                .min(16, "Debe ingresar 16 digitos")
+                .max(16, "Debe ingresar 16 digitos")
+                .required("Debe ingresar un numero de tarjeta"),
+            card_expires: Yup.date()
+                .min(new Date(), "La fecha de vencimiento no puede ser menor a la fecha actual")
+                .required("Debe ingresar una fecha de expiración"),
+            card_cvv: Yup.number()
+                .min(3, "Debe ingresar 3 digitos")
+                .max(3, "Debe ingresar 3 digitos")
+                .required("Debe ingresar un cvv"),
+                
+        }),
         onSubmit: async (values) => {
             dispatch(
                 addNewCardAction({
@@ -44,112 +58,108 @@ const NewCard = () => {
                     card_expires: values.card_expires,
                     card_cvv: values.card_cvv,
                 })
-                )
+            );
         },
     });
-
-
-
 
     return (
         <>
             <Toolbar className="toolBar">
                 <Grid>
-                    <IconButton aria-label="Regresar" align="left" onClick={() => navigate(-1)}>
-                        <ArrowBackIosNewIcon  />
+                    <IconButton
+                        aria-label="Regresar"
+                        align="left"
+                        onClick={() => navigate(-1)}
+                    >
+                        <ArrowBackIosNewIcon />
                     </IconButton>
                 </Grid>
                 <Container>
-                <Typography variant="h5" color="inherit" align="center">
-                    Agrega nueva tarjeta
-                </Typography>
+                    <Typography variant="h5" color="inherit" align="center">
+                        Agrega nueva tarjeta
+                    </Typography>
                 </Container>
             </Toolbar>
-                <Container maxWidth={'80'}>
+            <Container maxWidth={"80"}>
                 <Typography variant="subtitle1" color="inherit" align="center">
                     Completá los datos de la tarjeta de debito o crédito con la
                     que vas a pagar la reserva
                 </Typography>
-                </Container>
+            </Container>
             <Container>
-                <form onSubmit={handleSubmit} >
-                <Card className="inputContainer" elevation={2}>
-                    <Grid container spacing={3} justifyContent="center">
-                        <Grid item xs={10} md={6}>
-                            {/* <TextField
-                                required
-                                id="card_name"
-                                label="Nombre de la tarjeta"
-                                fullWidth
-                                autoComplete="cc-name"
-                                variant="standard"
-                                name="card_name"
-                                onChange={handleChange}
-                                value={values.card_name}
-                            /> */}
-                            <CustomInputForm
-                            key={0}
-                            onChange={handleChange}
-                            label="Nombre del titular de la tarjeta"
-                            name="card_name"
-                            value={values.card_name}
-                            type={"text"}
-                            placeholder={"Nombre de la tarjeta"}
-                            textError={errors.card_name}
-                        />
+                <form onSubmit={handleSubmit}>
+                    <Card className="inputContainer" elevation={2}>
+                        <Grid container spacing={3} justifyContent="center">
+                            <Grid item xs={10} md={6}>
+                                <TextField
+                                    required
+                                    id="card_name"
+                                    label="Nombre de la tarjeta"
+                                    fullWidth
+                                    autoComplete="cc-name"
+                                    variant="standard"
+                                    name="card_name"
+                                    onChange={handleChange}
+                                    value={values.card_name}
+                                />
+                            </Grid>
+                            <Grid item xs={10} md={6}>
+                                <TextField
+                                    required
+                                    id="card_number"
+                                    label="Número de la tarjeta"
+                                    fullWidth
+                                    autoComplete="cc-number"
+                                    variant="standard"
+                                    name="card_number"
+                                    onChange={handleChange}
+                                    value={values.card_number}
+                                />
+                            </Grid>
+                            <Grid item xs={10} md={6}>
+                                <TextField
+                                    required
+                                    id="card_expires"
+                                    label="Fecha de expiración"
+                                    fullWidth
+                                    autoComplete="cc-exp"
+                                    variant="standard"
+                                    name="card_expires"
+                                    onChange={handleChange}
+                                    value={values.card_expires}
+                                />
+                            </Grid>
+                            <Grid item xs={10} md={6}>
+                                <TextField
+                                    required
+                                    id="card_cvv"
+                                    label="CVV"
+                                    helperText="Últimos tres dígitos en la parte posterior de tu tarjeta"
+                                    fullWidth
+                                    autoComplete="cc-csc"
+                                    variant="standard"
+                                    name="card_cvv"
+                                    onChange={handleChange}
+                                    value={values.card_cvv}
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item xs={10} md={6}>
-                            <TextField
-                                required
-                                id="card_number"
-                                label="Número de la tarjeta"
-                                fullWidth
-                                autoComplete="cc-number"
-                                variant="standard"
-                                name="card_number"
-                                onChange={handleChange}
-                                value={values.card_number}
-                            />
-                        </Grid>
-                        <Grid item xs={10} md={6}>
-                            <TextField
-                                required
-                                id="card_expires"
-                                label="Fecha de expiración"
-                                fullWidth
-                                autoComplete="cc-exp"
-                                variant="standard"
-                                name="card_expires"
-                                onChange={handleChange}
-                                value={values.card_expires}
-                            />
-                        </Grid>
-                        <Grid item xs={10} md={6}>
-                            <TextField
-                                required
-                                id="card_cvv"
-                                label="CVV"
-                                helperText="Últimos tres dígitos en la parte posterior de tu tarjeta"
-                                fullWidth
-                                autoComplete="cc-csc"
-                                variant="standard"
-                                name="card_cvv"
-                                onChange={handleChange}
-                                value={values.card_cvv}
-                            />
-                        </Grid>
-                    </Grid>
-                </Card>
-                <Container className="buttonContainer" align="center">
-                    <Button variant="contained" className="button" type="submit">
-                        Guardar Tarjeta
-                    </Button>
-                    <CustomSubmitInputForm text={"Guardar Tarjeta"} />
-                </Container>
+                    </Card>
+                    <Container className="buttonContainer" align="center">
+                        <Button
+                            variant="contained"
+                            className="button"
+                            type="submit"
+                        >
+                            Guardar Tarjeta
+                        </Button>
+                    </Container>
                 </form>
             </Container>
         </>
     );
 };
+
+
 
 export default NewCard;
